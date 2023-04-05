ed0345d5d50a9642dfaef3ef75cb206fca6a6ba1
GeoSpatialEngine <121663021+GeoSpatialEngine@users.noreply.github.com>
2023-04-05 11:45:51 +0530

    p

---
+++ onlyoffice-strapi-server/config/middlewares.js
@@ -0,0 +1,23 @@
+module.exports = [
+  'strapi::errors',
+  {
+    name: "strapi::security",
+    config: {
+      contentSecurityPolicy: {
+        useDefaults: true,
+        directives: {
+          "script-src": ["'self'", "https:", "http:"],
+          "frame-src": ["'self'", "https:", "http:"],
+        },
+      },
+    },
+  },
+  'strapi::cors',
+  'strapi::poweredBy',
+  'strapi::logger',
+  'strapi::query',
+  'strapi::body',
+  'strapi::session',
+  'strapi::favicon',
+  'strapi::public',
+];
